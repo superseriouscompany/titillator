e7fb3e3b4a9f385924660aff56300cacc4a0514e
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-04-19 17:53:25 +0100

    Show the 'I dont know this person' link throughout

--- src/views/OctagonView.js
+++ src/views/OctagonView.js
@@ -11,18 +11,14 @@ export default function(props) {
               <img src={props.blue.avatar_url} alt={`%{props.blue.name} on LinkedIn`} className="face"/>
               <div>{props.blue.name}</div>
             </div>
-            { props.round === 0 ?
-              <a className="remove" onClick={() => props.remove(props.blue.id)}>I don't know {props.blue.name}</a>
-            : null }
+            <a className="remove" onClick={() => props.remove(props.blue.id)}>I don't know {props.blue.name}</a>
           </div>
           <div className="red corner">
             <div className="card" onClick={() =>  props.choose(props.red.id, props.blue.id)}>
               <img src={props.red.avatar_url} alt={`%{props.red.name} on LinkedIn`} className="face"/>
               <div>{props.red.name}</div>
             </div>
-            { props.round === 0 ?
-              <a className="remove" onClick={() => props.remove(props.red.id)}>I don't know {props.red.name}</a>
-            : null }
+            <a className="remove" onClick={() => props.remove(props.red.id)}>I don't know {props.red.name}</a>
           </div>
         </div>
       : null }
