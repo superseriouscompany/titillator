268d14961531b9a59afc1c5c2de46770b48a6cfe
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-04-19 11:32:54 +0100

    Split out OctagonView

--- src/components/Octagon.js
+++ src/components/Octagon.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import OctagonView from '../views/OctagonView'
 
 class Octagon extends Component {
   constructor(props) {
@@ -33,25 +34,7 @@ class Octagon extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Round {this.props.round + 1}</h2>
-        { !!this.props.blue || !!this.props.red ?
-          <div className="octagon">
-            <div className="blue corner" onClick={() => this.choose(this.props.blue.id, this.props.red.id)}>
-              <img src={this.props.blue.avatar_url} alt={`%{this.props.blue.name} on LinkedIn`} className="face"/>
-              <div>
-                <div>{this.props.blue.name}</div>
-              </div>
-            </div>
-            <div className="red corner" onClick={() =>  this.choose(this.props.red.id, this.props.blue.id)}>
-              <img src={this.props.red.avatar_url} alt={`%{this.props.red.name} on LinkedIn`} className="face"/>
-              <div>
-                <div>{this.props.red.name}</div>
-              </div>
-            </div>
-          </div>
-        : null }
-      </div>
+      <OctagonView {...this.props} />
     );
   }
 }
---
+++ src/views/OctagonView.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function(props) {
+  return (
+    <div>
+      <h2>Round {props.round + 1}</h2>
+      { !!props.blue || !!props.red ?
+        <div className="octagon">
+          <div className="blue corner" onClick={() => this.choose(props.blue.id, props.red.id)}>
+            <img src={props.blue.avatar_url} alt={`%{props.blue.name} on LinkedIn`} className="face"/>
+            <div>
+              <div>{props.blue.name}</div>
+            </div>
+          </div>
+          <div className="red corner" onClick={() =>  this.choose(props.red.id, props.blue.id)}>
+            <img src={props.red.avatar_url} alt={`%{props.red.name} on LinkedIn`} className="face"/>
+            <div>
+              <div>{props.red.name}</div>
+            </div>
+          </div>
+        </div>
+      : null }
+    </div>
+  )
+}
