641f06ec9d12e4a4761d869d62e747f36cc17a03
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-04-19 11:52:25 +0100

    Add profile reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import {combineReducers, createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger'
 
 import matchup from './matchup'
-import scene from './scene'
+import scene   from './scene'
+import profile from './profile'
 
 const reducers = combineReducers({
   matchup,
   scene,
+  profile,
 })
 
 export default createStore(reducers, applyMiddleware(logger))
